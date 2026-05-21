84fabe6a142328336b1479717ae8d30680924370
. <h@>
2026-05-21 10:30:16 -0400

    correct student insert format for sql

--- src/Students.java
+++ src/Students.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 public class Students {
     private String lastName;
     private String firstName;
+    private int studentID;
+    private static int studentCounter = 0;
 
     public Students(String fName, String lName){
-
         this.firstName = fName;
         this.lastName = lName;
+        studentCounter++;
+        studentID = studentCounter;
     }
+
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/Students");
         ArrayList<Students> listOfStudents = new ArrayList<>();
+        System.out.println("insert into students (firstName, lastName) values ");
         for (int i = 0; i < fileData.size(); i++) {
             String first = fileData.get(i).split(" ")[0];
             String last = fileData.get(i).split(" ")[1];
@@ -24,12 +29,16 @@ public class Students {
         for (int i = 0; i < listOfStudents.size(); i++) {
             System.out.println(listOfStudents.get(i));
         }
+
     }
 
     @Override
     public String toString() {
-        return ("INSERT INTO Students (firstName,lastName) " +
-                "VALUES (\""+firstName+"\",\""+lastName+"\");");
+        if (studentID == 5000){
+            return ("(\"" + firstName + "\",\"" + lastName + "\");");
+        } else {
+            return ("(\""+firstName+"\",\""+lastName+"\"),");
+        }
     }
 
     public static ArrayList<String> getFileData(String fileName) {
