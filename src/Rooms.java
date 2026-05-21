b6027a9fe04d1220e959978f4cfd32d7cb460d56
. <h@>
2026-05-21 14:10:10 -0400

    fixed rooms

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,9 +1,32 @@
 public class Rooms {
 
-    public Rooms (int floor, int direction, int roomNum){
+    private final char direction;
+    private char floor;
+    private char wing;
+    private int roomNum;
+    private static char[] directions = new char[]{'N','S','E','W'};
+    private static int roomIDCounter = 0;
+    private int roomID;
 
+    public Rooms(int floor, int direction, int rNum) {
+        this.floor = (floor == 0)? 'B' : (char) (floor + '0');
+        this.direction = directions[direction];
+        this.roomNum = rNum;
+        roomIDCounter++;
+        roomID = roomIDCounter;
     }
     public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < directions.length; j++) {
+                for (int k = 1; k < 21; k++) {
+                    System.out.println(new Rooms(i,j,k));
+                }
+            }
+        }
+    }
 
+    @Override
+    public String toString() {
+        return ("Insert into Rooms (floor, wing, roomNum) " + "VALUES ("+floor+",\""+direction+"\","+roomNum+");");
     }
 }
