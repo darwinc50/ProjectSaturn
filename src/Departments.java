1abe6adafa7e90a3a07d8541f34ad6413acbf47e
. <h@>
2026-05-21 10:11:24 -0400

    I think i did this part right

--- src/Departments.java
+++ src/Departments.java
@@ -1,5 +1,45 @@
-public class Departments {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Departments {
+    private String departmentName;
+
+    public Departments(String depName){
+        this.departmentName = depName;
+    }
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/Departments");
+        ArrayList<Departments> departments = new ArrayList<>();
+        for (int i = 0; i < fileData.size(); i++) {
+            Departments d = new Departments(fileData.get(i));
+            departments.add(d);
+        }
+        for (Departments d : departments){
+            System.out.println(d);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ("Insert into Departments (departmentName) " + "VALUES (\""+departmentName+"\");");
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
     }
 }
--- src/Students.java
+++ src/Students.java
@@ -1,5 +1,51 @@
-public class Students {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Students {
+    private String lastName;
+    private String firstName;
+
+    public Students(String fName, String lName){
+
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/Students");
+        ArrayList<Students> listOfStudents = new ArrayList<>();
+        for (int i = 0; i < fileData.size(); i++) {
+            String first = fileData.get(i).split(" ")[0];
+            String last = fileData.get(i).split(" ")[1];
+            Students s = new Students(first, last);
+            listOfStudents.add(s);
+        }
+        for (int i = 0; i < listOfStudents.size(); i++) {
+            System.out.println(listOfStudents.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ("INSERT INTO Students (firstName,lastName) " +
+                "VALUES (\""+firstName+"\",\""+lastName+"\");");
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
     }
 }
