720a0d08e8a8c134f0eace3dc9c4c2d85b91ab73
d <clxmr3@gmail.com>
2026-05-26 20:56:58 -0400

    Teachers done

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,24 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Students.generatesStudents();
-//        Students.printStudents();
-//        System.out.println();
-//        Rooms.generateRooms();
-//        Rooms.printRooms();
-//        System.out.println();
-//        Departments.generateDepartments();
-//        Departments.printDepartments();
-//        Courses.generateCourses();
-//        Courses.printCourses();
+        Students.generatesStudents();
+        Students.printStudents();
+        System.out.println();
+
+        Rooms.generateRooms();
+        Rooms.printRooms();
+        System.out.println();
+
+        Departments.generateDepartments();
+        Departments.printDepartments();
+        System.out.println();
+
+        Courses.generateCourses();
+        Courses.printCourses();
+        System.out.println();
+
         Teachers.generateTeachers();
         Teachers.printTeachers();
+        System.out.println();
     }
 }
